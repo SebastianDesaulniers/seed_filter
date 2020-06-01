eb6b49caeb9de48f58351710999cab0f0ccd8377
Sebastian Desaulniers <desaulniers.sebastian@gmail.com>
2020-06-01 17:38:40 -0400

    version 0.2

--- seed_filter.js
+++ seed_filter.js
@@ -1,18 +1,36 @@
-var NATURES = { "hardy": 0, "lonely": 1, "brave": 2, "adamant": 3, "naughty": 4, "bold": 5, "docile": 6, "relaxed": 7, "impish": 8, "lax": 9, "timid": 10, "hasty": 11, "serious": 12, "jolly": 13, "naive": 14, "modest": 15, "mild": 16, "quiet": 17, "bashful": 18, "rash": 19, "calm": 20, "gentle": 21, "sassy": 22, "careful": 23, "quirky": 24 }
-var NO_HAIL = {4: [5, 6, 7, 8, 11, 12, 15, 16, 17, 18, 21, 22, 23, 25, 26, 27, 28, 29],
-              8: [3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17, 20, 21, 22, 25, 26, 27, 28, 31],
-              12: [4, 5, 6, 7, 10, 11, 15, 16, 17, 19, 20, 21, 25, 26, 27, 28, 31]}
-function range (start, end) { return [...Array(1+end-start).keys()].map(v => start+v) } // thanks SO (https://stackoverflow.com/questions/36947847/how-to-generate-range-of-numbers-from-0-to-n-in-es2015-only)
-var SLOTS = {0: range(0, 19), 1: range(20, 39), 2: range(40,49), 3: range(50,59), 4: range(60,69), 5: range(70, 79), 6: range(80, 84), 7: range(85, 89),   8: range(90, 93), 9: range(94, 97), 10: [98], 11: [99]} 
-    
+const NATURES = { "hardy": 0, "lonely": 1, "brave": 2, "adamant": 3, "naughty": 4, "bold": 5, "docile": 6, "relaxed": 7, "impish": 8, "lax": 9, "timid": 10, "hasty": 11, "serious": 12, "jolly": 13, "naive": 14, "modest": 15, "mild": 16, "quiet": 17, "bashful": 18, "rash": 19, "calm": 20, "gentle": 21, "sassy": 22, "careful": 23, "quirky": 24 }
+const INV_NATURES = ["hardy", "lonely", "brave", "adamant", "naughty", "bold", "docile", "relaxed", "impish", "lax", "timid", "hasty", "serious", "jolly", "naive", "modest", "mild", "quiet", "bashful", "rash", "calm", "gentle", "sassy", "careful", "quirky"]
+const BW_VALID_DATES = {
+    4: [5, 6, 7, 8, 11, 12, 15, 16, 17, 18, 21, 22, 23, 25, 26, 27, 28, 29],
+    8: [3, 4, 5, 6, 7, 12, 13, 14, 15, 16, 17, 20, 21, 22, 25, 26, 27, 28, 31],
+    12: [4, 5, 6, 7, 10, 11, 15, 16, 17, 19, 20, 21, 25, 26, 27, 28, 31]
+}
+function range(start, end) { return [...Array(1 + end - start).keys()].map(v => start + v) } // thanks SO (https://stackoverflow.com/questions/36947847/how-to-generate-range-of-numbers-from-0-to-n-in-es2015-only)
+const ENCOUNTER_SLOTS = { 0: range(0, 19), 1: range(20, 39), 2: range(40, 49), 3: range(50, 59), 4: range(60, 69), 5: range(70, 79), 6: range(80, 84), 7: range(85, 89), 8: range(90, 93), 9: range(94, 97), 10: [98], 11: [99] }
 
 class Seed {
-    constructor(seed) {
-        this.initial_seed = seed
+    constructor(seed, stepcount) {
+        seed = BigInt(seed)
+
+        this.initialSeed = seed
         this.current = seed
-        this.current_frame = 0
-        this.frames = new Array(1000)
-        this.frames[0] = this.initial_seed
+        this.currentFrame = 0
+
+        this.frames = new Array(5000)
+        this.frames[0] = this.initialSeed
+
+        this.stepcount = stepcount
+    }
+    step() {
+        this.stepcount++
+        this.advance(2)
+        if (this.stepcount > 127) {
+            this.advance(1)
+            this.stepcount = 0
+        }
+        if (this.stepcount % 20 == 0) {
+            this.advance(1)
+        }
     }
 
     high16() {
@@ -23,72 +41,146 @@ class Seed {
         return (this.current >> 32n) & 0xFFFFn;
     }
 
-    get_pid() {
-        return BigInt(parseInt(this.high16().toString(16) + "" + this.low16().toString(16), 16))
-    }
-
     advance(n) {
         for (var i = 0; i < n; i++) {
-            this.current = ((this.current * 0x5D588B656C078965n) & 0xFFFFFFFFFFFFFFFFn) + 0x269EC3n;
-            this.frames[++this.current_frame] = this.current
+            this.current = ((this.current * 0x5D588B656C078965n) & 0xFFFFFFFFFFFFFFFFn) + 0x269EC3n
+            this.frames[++this.currentFrame] = this.current
         }
     }
-    get_nature() {
-        var temp = ((this.current * 0x5D588B656C078965n) & 0xFFFFFFFFFFFFFFFFn) + 0x269EC3n; 
-        return ((temp >> 32n)* 25n) >> 32n
+
+    next(n) {
+        let temp = this.current
+        for (var i = 0; i < n; i++) {
+            temp = ((temp * 0x5D588B656C078965n) & 0xFFFFFFFFFFFFFFFFn) + 0x269EC3n
+        }
+        return temp;
     }
 
-    is_encounter(n) {
-        return (this.frames[n] >> 48n) / 656n <= 8n
+    getPID() {
+        let l = this.low16().toString(16)
+        return BigInt("0x" + this.high16().toString(16) + "" + l);
     }
-    slot(n) { // forgive me wartab 
+
+    getAbility() {
+        return Number(~(this.getPID() >> 16n) & 1n)
+    }
+
+    getNature() {
+        let res = this.next(1)
+        return Number(((res >> 32n) * 25n) >> 32n)
+    }
+
+    getEncounterSlot(n) { // Forgive me, wartab 
         var res = Number((this.frames[n] >> 48n) / 656n)
-        console.log(res)
-        for (var i =0; i < 12; i++) {
-            if (SLOTS[i].includes(res))
+        for (var i = 0; i <= 11; i++) {
+            if (ENCOUNTER_SLOTS[i].includes(res))
                 return i;
         }
     }
+
+    canSpawnCloud() {
+        let res = this.next(1)
+        return (res >> 60n) == 0n && BigInt(this.stepcount) % 20n == 0n
+    }
+
+    hasDustCloudEncounter() {
+        let res = (((this.current >> 32n) * 1000n) >> 32n)
+        return res <= 400n
+    }
+
+    hasEncounter(n) {
+        return (this.frames[n] >> 48n) / 656n <= 8n
+    }
+
 }
 
+function parseText(id, key, func, def) {
+    let res = document.getElementById(id).value.split(key)
+    if (func != undefined)
+        res = res.map(x => func(x))
+
+    if (res.includes("-1") || res.includes(-1)) return def;
+    return res;
+}
+
+
 function filter() {
-    var natures = document.getElementById("natures").value.split(" ")
-    document.getElementById("output").value = "";
+    document.getElementById("output").value = ""
+
+    var natures = parseText("natures", " ", undefined, "adamant bashful bold brave calm careful docile gentle hardy hasty impish jolly lax lonely mild modest naive naughty quiet quirky rash relaxed sassy serious timid".split(" "))
     for (var i = 0; i < natures.length; i++) {
         natures[i] = NATURES[natures[i].toLowerCase()]
     }
-    var slots = document.getElementById("slots").value.split(" ").map(x => parseInt(x))
-    var abilities = document.getElementById("abilities").value.split(" ").map(x => parseInt(x))
-    var genders = document.getElementById("genders").value.split(" ")
 
-    var frames = document.getElementById("frames").value.split("/")
-    var min_frame = parseInt(frames[0])
-    var max_frame = parseInt(frames[1])
+    var slots = parseText("slots", " ", parseInt, range(0, 11))
+    var abilities = parseText("abilities", " ", parseInt, [0, 1])
+    var genders = parseText("genders", " ", parseInt, [0, 1, 2])
 
-    var hail = document.getElementById("hail").value.toLowerCase() == "y"
-    var months = document.getElementById("months").value.split(" ").map(x => parseInt(x))
-    var hours = document.getElementById("hours").value.split(" ").map(x => parseInt(x))
-    var seconds = document.getElementById("seconds").value.split(" ").map(x => parseInt(x))
+    var frames = parseText("frames", "/", parseInt, [30, 300])
+    var minFrame = frames[0], maxFrame = frames[1]
+
+    var bw1 = parseText("bw1", "", x => x.toLowerCase() == "y", "y")[0]
+    var dustCloud = parseText("dustCloud", "", x => x.toLowerCase() == "y", "y")[0]
+    var hasDustCloud = false
 
 
-    var raw_data = document.getElementById("input").value.split("\n")
-    for (var i = 1; i < raw_data.length - 1; i++) {
-        var data = raw_data[i].split('\t')
+    var months = parseText("months", " ", parseInt, range(1, 12))
+    var days = parseText("days", " ", parseInt, range(1, 31))
 
-        var dates = data[10].split('/'), month=parseInt(dates[0]), day=parseInt(dates[1]), year=parseInt(dates[2].split(' ')[0])
-        var times = ((data[10].split(' ')).join(':')).split(':'), hour=parseInt(times[1]), minute=parseInt(times[2]), second=parseInt(times[3])
+    var hours = parseText("hours", " ", parseInt, range(0, 23))
+    var minutes = parseText("minutes", " ", parseInt, range(0, 59))
+    var seconds = parseText("seconds", " ", parseInt, range(0, 59))
 
-        if (!hail && !NO_HAIL[month].includes(day))
+    var rawData = document.getElementById("input").value.split("\n")
+
+    for (var i = 0; i < rawData.length; i++) {
+        if (rawData[i].length <= 7 || rawData[i][0] == "S") continue
+
+        var data = rawData[i].split('\t')
+        var dates = data[10].split('/'), month = parseInt(dates[0]), day = parseInt(dates[1]), year = parseInt(dates[2].split(' ')[0])
+        var times = ((data[10].split(' ')).join(':')).split(':'), hour = parseInt(times[1]), minute = parseInt(times[2]), second = parseInt(times[3])
+
+        if (bw1 && !BW_VALID_DATES[month].includes(day))
             continue
+        if (!months.includes(month) || !days.includes(day) || !hours.includes(hour) || !seconds.includes(second) || !minutes.includes(minute))
+            continue;
 
-        var seed = new Seed(BigInt("0x" + data[0]))
-        seed.advance(min_frame)
-        for (var j = min_frame;  j <= max_frame; j++) {
-            if (abilities.includes(~(Number(seed.get_pid()) >> 16) & 1) && natures.includes(Number(seed.get_nature())) && seed.is_encounter(j - 3) && slots.includes(seed.slot(j - 2))){
-                console.log(seed);
-                document.getElementById("output").value += "PIDRNG FRAME: " + j + " " + raw_data[i] + "\n" // forgive me wartab
+        hasDustCloud = false
+
+        var seed = new Seed("0x" + data[0], 90)
+        var extraSteps = 0, encounterFrame = 0, extraAdvances = 0
+        seed.advance(minFrame)
+
+        for (var j = minFrame; j <= maxFrame; j++) {
+            if (dustCloud && seed.currentFrame <= maxFrame) {
+                if (hasDustCloud) {
+                    if (seed.hasDustCloudEncounter() && abilities.includes(seed.getAbility()) && natures.includes(seed.getNature()) && slots.includes(seed.getEncounterSlot(seed.currentFrame - 2))) {
+                        document.getElementById("output").value += INV_NATURES[seed.getNature()] + " ADVANCES: " + (seed.currentFrame - encounterFrame + 3) + " FRAME: " + Number(encounterFrame) + " " +  rawData[i] + "\n" // Forgive me, wartab x2
+                        break;
+                    }
+                    else {
+                        seed.advance(2)
+                        extraAdvances++
+                    }
+
+                }
+                else {
+                    seed.step()
+                    if (seed.canSpawnCloud()) {
+                        hasDustCloud = true
+                        encounterFrame = seed.currentFrame
+                    }
+                }
             }
-            seed.advance(1)
+            else if (!dustCloud) {
+                if (seed.hasEncounter(seed.currentFrame - 3)) {
+                    if (abilities.includes(Number(seed.getAbility())) && natures.includes(Number(seed.getNature())) && slots.includes(Number(seed.getEncounterSlot(seed.currentFrame - 2)))) {
+                        document.getElementById("output").value += INV_NATURES[seed.getNature()] + " FRAME: " + Number(seed.currentFrame) + " " +  rawData[i] + "\n" // Forgive me, wartab x2
+                        break;
+                    }
+                }
+                seed.advance(1)
+            }
         }
     }
 
